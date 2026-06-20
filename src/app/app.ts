31295cbe6b6c94aea41dbef156625d02cd93e344
VimarshaaVJain <vimarshajain1l@gmail.com>
2026-06-20 13:41:51 +0530

    component basics with cli commands

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
+import { Login } from './login/login';
 // import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  // imports: [RouterOutlet],
+  imports: [Login],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
